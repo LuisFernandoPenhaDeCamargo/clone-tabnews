3662407ac8288d411a075b2be84dac8dd48731ac
Luis Fernando Penha de Camargo <luisfernandopenhadecamargo@gmail.com>
2025-11-28 18:23:12 -0300

    test: testing Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Teste</h1>
+    return (
+        <>
+            <h1>Luis Fernando Penha de Camargo</h1>
+            <p>I write code. Sometimes it even works. I try really hard to make those times more frequent.</p>
+        </>
+    );
 }
 
 export default Home;
